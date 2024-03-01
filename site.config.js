9ad6654031833895f28d59c3a0b737cd33cae9af
rose <hyobinkim@gmail.com>
2024-03-01 16:32:45 +0900

    modified:   public/apple-touch-icon.png 	deleted:    public/avatar.svg 	modified:   public/favicon.ico 	new file:   public/profile.png 	modified:   site.config.js

--- site.config.js
+++ site.config.js
@@ -1,31 +1,31 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "rose",
+    image: "/profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "M.S. Department of Applied Artificial Intelligence",
+    bio: "도비의 대학원 탈출일기...",
+    email: "hyobinkim@gmail.com",
+    linkedin: "hyobin-kim-0993ab1b1",
+    github: "rosakim83",
+    instagram: "rosa_kim83",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `rose-log`,
+      href: "https://github.com/rosakim83/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "rose-log",
+    description: "welcome to rose-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://rose-log.vercel.app",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
